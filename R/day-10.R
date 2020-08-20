48fe525a12a65e96dfa0b053384201b4e76d7346
XuefeiG <xuefei@ucsb.edu>
2020-08-21 01:16:22 +0800

    x

--- R/day-10.R
+++ R/day-10.R
@@ -7,8 +7,14 @@ library(sf)
 CONUS <- us_states %>%
   filter(name != "Alaska" & name != "Puerto Rico" & name != "Hawaii")
 
+CONUS_ls0 <- CONUS %>%
+  st_combine() %>%
+  st_cast("MULTILINESTRING")
+CONUS_ls0
+plot(CONUS_ls0)
+
 CONUS_ls <- CONUS %>%
   st_union() %>%
   st_cast("MULTILINESTRING")
-CONUS_ls2
+CONUS_ls
 plot(CONUS_ls)
