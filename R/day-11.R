c395d145cf9dd3a7de64ada0fa8d1457830dd9f9
XuefeiG <xuefei@ucsb.edu>
2020-08-25 05:37:37 +0800

    11

--- R/day-11.R
+++ R/day-11.R
@@ -5,7 +5,7 @@ cities = readr::read_csv("data/uscities.csv") %>%
   st_as_sf(coords = c("lng", "lat"), crs = 4326)
 
 mycity <- cities %>%
-  filter(city %in% c("Santa Barbara", "Buffalo"))
+  filter(city %in% c("Santa Barbara", "Rio"))
 
 equal <- st_transform(mycity, 5070)
 equidistant <- st_transform(mycity, '+proj=eqdc +lat_0=40 +lon_0=-96 +lat_1=20 +lat_2=60 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs')
